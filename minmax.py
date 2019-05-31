d89a3538ab93b71d475f10ff708e3a54ab6193f2
Victor Gabriel <victorgfb80@gmail.com>
2019-05-31 09:48:03 -0300

    corrige maxValue e minValue

--- minmax.py
+++ minmax.py
@@ -18,16 +18,38 @@ class MinMax:
         v  = -9999999
 
         for filho in  no.funcaoSucessora(simbolo):
-            v = max(v, self.minValue(filho,simbolo))
+            if(filho.testeDeTermino):
+                if(count == None):
+                    count = 0
+                count +=  self.maxValue(filho,simbolo)
+            else:
+                v = max(v, self.minValue(filho,simbolo))
+        
+        if(count != None):
+            v = count
+        return v
 
     def minValue(self, no, simbolo):
         if(no.testeDeTermino):
             return no.funcaoUtilidade()
 
         v  = 9999999
+        count = None
 
         for filho in  no.funcaoSucessora(simbolo):
-            v = min(v, self.maxValue(filho,simbolo))
+            if(filho.testeDeTermino):
+                if(count == None):
+                    count = 0
+                count +=  self.maxValue(filho,simbolo)
+            else:
+                v = min(v, self.maxValue(filho,simbolo))
+
+        if(count != None):
+            v = count
+        return v
+
+    def miniMaxDecision(self, no, simbolo):
+        v = self.maxValue(no,simbolo)
 
 aux = MinMax()
 #print(aux.testeDeTermino([['X','O','X'],['O','X','O'],['O','X','O']]))
